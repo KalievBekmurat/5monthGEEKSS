ef48e223a65fe49353075c43f563df120fdb1d2a
KalievBekmurat <kalievbekmurat2@gmail.com>
2023-10-29 21:55:06 +0300

    2 and 3 homeworks

--- product/models.py
+++ product/models.py
@@ -1,24 +1,36 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Category(models.Model):
-    name = models.CharField(max_length=100,null=True)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
+
+
 class Product(models.Model):
-    title = models.CharField(max_length=100,null=True)
+    title = models.CharField(max_length=100)
     description = models.TextField(null=True)
-    price = models.IntegerField(null=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL,null=True)
+    price = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
 
 
+    def __str__(self):
+        return self.title
+
+
+STARS = (
+    (1, '*' *1),
+    (2, '*' *2),
+    (3, '*' *3),
+    (4, '*' *4),
+    (5, '*' *5),
+)
 class Review(models.Model):
-    text = models.TextField(max_length=1000,null=True)
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL,null=True)
+    text = models.TextField(null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, related_name='reviews')
+    stars = models.IntegerField(choices=STARS, default=1)
 
     def __str__(self):
         return self.text
--- product/views.py
+++ product/views.py
@@ -5,53 +5,149 @@ from product.serializers import CategorySerializer, ProductSerializer, ReviewSer
 
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def category_list_api_view(request):
-    categories = Category.objects.all()
-    categoryes_json = CategorySerializer(categories, many=True).data
-    return Response(data=categoryes_json)
 
-@api_view(['GET'])
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        categoryes_json = CategorySerializer(categories, many=True).data
+        return Response(data=categoryes_json)
+
+    elif request.method == 'POST':
+        #1
+        name = request.data.get('name')
+        #2
+        create_category = Category.objects.create(name=name)
+        create_category.save()
+        #3
+        return Response(status=201,
+                        data={'message': 'Category created',
+                              'create_category': {'id': create_category.id,
+                                                  'name': create_category.name}})
+
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def category_detail_api_view(request, category_id):
     try:
         category = Category.objects.get(id=category_id)
     except Category.DoesNotExist:
         return Response(data={'message': 'Category not Found!'}, status=404)
-    category_json = CategorySerializer(category).data
-    return Response(data=category_json)
+
+    if request.method == 'GET':
+        category_json = CategorySerializer(category).data
+        return Response(data=category_json)
+
+    elif request.method == 'PUT':
+        category.name = request.data.get('name')
+        category.save()
+        return Response(status=201,
+                        data={'message': 'Category updated'})
+
+    elif request.method == 'DELETE':
+        category.delete()
+        return Response(status=204,
+                        data={'message': 'Category Destoyed'})
+
 
 
 
 
 @api_view(['GET'])
 def product_list_api_view(request):
-    products = Product.objects.all()
-    products_json = CategorySerializer(products, many=True).data
-    return Response(data=products_json)
+    if request.method == 'GET':
+        products = Product.objects.all()
+        products_json = ProductSerializer(products, many=True).data
+        return Response(data=products_json)
 
-@api_view(['GET'])
+    elif request.method == 'POST':
+        #1
+        title = request.data.get('title')
+        description = request.data.get('description')
+        price = request.data.get('price')
+        category_id = request.data.get('category_id')
+        #2
+        create_product = Product.objects.create(title=title, description=description,
+                                                 price=price, category_id=category_id)
+        create_product.save()
+        #3
+        return Response(status=201,
+                        data={'message': 'Product created',
+                              'create_product': {'id': create_product.id,
+                                                 'title': create_product.title}})
+
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail_api_view(request, product_id):
     try:
         product = Product.objects.get(id=product_id)
     except Product.DoesNotExist:
         return Response(data={'message': 'Product not Found!'}, status=404)
-    product_json = ProductSerializer(product).data
-    return Response(data=product_json)
+    if request.method == 'GET':
+        product_json = ProductSerializer(product).data
+        return Response(data=product_json)
+
+    elif request.method == 'PUT':
+        product.title = request.data.get('title')
+        product.description = request.data.get('description')
+        product.price = request.data.get('price')
+        product.category_id = request.data.get('category_id')
+        product.save()
+        return Response(status=201,
+                        data={'message': 'Product updated'})
+
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=204,
+                        data={'message': 'Product Destoyed'})
+
 
 
 
 
 @api_view(['GET'])
 def review_list_api_view(request):
-    reviews = Review.objects.all()
-    reviews_json = ReviewSerializer(reviews, many=True).data
-    return Response(data=reviews_json)
+    if request.method == 'GET':
+        reviews = Review.objects.all()
+        reviews_json = ReviewSerializer(reviews, many=True).data
+        return Response(data=reviews_json)
 
-@api_view(['GET'])
+    elif request.method == 'POST':
+        #1
+        text = request.data.get('text')
+        product_id = request.data.get('product_id')
+        stars = request.data.get('stars')
+        #2
+        create_review = Review.objects.create(text=text, product_id=product_id, stars=stars)
+        create_review.save()
+        #3
+        return Response(status=201,
+                        data={'message': 'Review created',
+                              'create_review': {'id': create_review.id,
+                                                'title': create_review.text}})
+
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def review_detail_api_view(request, review_id):
     try:
         review = Review.objects.get(id=review_id)
     except Review.DoesNotExist:
         return Response(data={'message': 'Review not Found!'}, status=404)
-    review_json = ReviewSerializer(review).data
-    return Response(data=review_json)
+    if request.method == 'GET':
+        review_json = ReviewSerializer(review).data
+        return Response(data=review_json)
+
+    elif request.method == 'PUT':
+        review.text = request.data.get('text')
+        review.product_id = request.data.get('product_id')
+        review.stars = request.data.get('stars')
+        review.save()
+        return Response(status=201,
+                        data={'message': 'Review updated'})
+
+    elif request.method == 'DELETE':
+        review.delete()
+        return Response(status=204,
+                        data={'message': 'Review Destoyed'})
